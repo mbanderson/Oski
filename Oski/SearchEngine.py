5e079ae31f1c2cd6a59e8e2d5a318427fac0ae00
mbanderson <mbanderson@ucla.edu>
2017-08-27 15:38:14 -0700

    Filter banned domains from search results. Combined engine+key files.

--- Oski/SearchEngine.py
+++ Oski/SearchEngine.py
@@ -5,23 +5,44 @@ import argparse
 import json
 import os
 import sys
+import tldextract
 from pprint import pprint
 from googleapiclient.discovery import build
+from urlparse import urlparse
+from Archiver import save_article
 
 
 def to_ascii(text):
     """Convert text input to ascii."""
     return text.encode('ascii', errors='ignore')
 
-def read_engine_json(json_file, key_str):
+def parse_domain(url):
+    """Parses domain name from url string."""
+    ext = tldextract.extract(url)
+    return ext.domain
+
+def rem_banned_domains(results, banned):
+    """Removes search results from banned sites."""
+    check = lambda result: parse_domain(result.url).lower() not in banned
+    return filter(check, results)
+
+def read_banned_domains(banned_file):
+    """Read text file of domains to filter from search results."""
+    if os.path.exists(banned_file):
+        with open(banned_file, 'r') as f:
+            content = f.readlines()
+            return [domain.strip() for domain in content]
+    return []
+    
+def read_engine_json(json_file, key_str, engine_str):
     """Read value of key_str in json_file."""
     if os.path.exists(json_file):
         with open(json_file, 'r') as f:
             content = json.load(f)
             try:
-                return content[key_str]
+                return content[key_str], content[engine_str]
             except KeyError:  pass
-    return ""
+    return "", ""
 
 
 class SearchEngine:
@@ -79,27 +100,40 @@ def main(args):
     """Run input query in search engine."""
     engine = SearchEngine(args.dev_key, args.engine_id)
     results = engine.query(args.query, args.results)
+    results = rem_banned_domains(results, args.banned_domains)
+
     pprint(results)
     print "Found %d results." % len(results)
+
+    if args.save:
+        for article in results:
+            save_article(article.url, article.title)
+
     return
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     parser = argparse.ArgumentParser()
-    parser.add_argument("-kf", "--keyfile", type=str, required=True,
-                        help="specify file containing Google API key")
     parser.add_argument("-ef", "--enginefile", type=str, required=True,
                         help="specify file containing search engine parameters")
     parser.add_argument("-q", "--query", type=str, default="Cal Kicker",
                         help="specify search query")
     parser.add_argument("-r", "--results", type=int, default=10,
                         help="specify number of results")
+    parser.add_argument("-bf", "--banfile", type=str,
+                        help="specify banned domain files")
+    parser.add_argument("--save", action="store_true",
+                        help="flag to save results as pdfs")
     args = parser.parse_args()
 
-    args.dev_key = read_engine_json(args.keyfile, "dev_key")
-    args.engine_id = read_engine_json(args.enginefile, "engine_id")
-    if args.dev_key and args.engine_id:
-        main(args)
-    else:
+    [args.dev_key, args.engine_id] = read_engine_json(args.enginefile, 
+                                                      "dev_key", "engine_id")
+    if not (args.dev_key and args.engine_id):
         print "SearchEngine: Not able to read input key files."
         sys.exit(1)
+    
+    args.banned_domains = []
+    if args.banfile:
+        args.banned_domains = read_banned_domains(args.banfile)
+
+    main(args)
