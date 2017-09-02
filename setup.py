71333aa6d19fc7178115c43732d9ca5020009b11
mbanderson <mbanderson@ucla.edu>
2017-09-02 16:04:16 -0700

    Added module files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Oski',
+    version='1.0',
+    description='Oski archives articles of Cal football placekickers.',
+    url='https://github.com/mbanderson/Oski',
+    packages=find_packages()
+)
